bd9e49a20f3aea4ca8fdb5dfc87c83344dc19a23
dassshic <goryaeva.daria@gmail.com>
2022-10-11 20:20:40 +0300

    hw

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName);
+        fullName = fullName.replace("IVANOV IVAN IVANOVICH", "Иванов Семён Семёнович");
+        fullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника — " + fullName);
     }
 }
